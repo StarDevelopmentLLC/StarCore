4144b027c2717334321390146d3d8aee54fc3014
Firestar311 <slaterandrew0398@gmail.com>
2023-12-01 09:43:18 -0600

    Basic setup and added the taskfactory and clock manager to teh services manager

---
+++ src/main/java/com/stardevllc/starcore/StarCore.java
@@ -0,0 +1,16 @@
+package com.stardevllc.starcore;
+
+import com.stardevllc.starclock.ClockManager;
+import com.stardevllc.starlib.task.TaskFactory;
+import com.stardevllc.starmclib.task.SpigotTaskFactory;
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StarCore extends JavaPlugin {
+    public void onEnable() {
+        getServer().getServicesManager().register(TaskFactory.class, new SpigotTaskFactory(this), this, ServicePriority.Normal);
+        ClockManager clockManager = new ClockManager(getLogger(), 50L);
+        getServer().getServicesManager().register(ClockManager.class, clockManager, this, ServicePriority.Normal);
+        getServer().getScheduler().runTaskTimer(this, clockManager.getRunnable(), 1L, 1L);
+    }
+}
