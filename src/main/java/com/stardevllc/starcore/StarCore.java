a85c7b3531524f98ea60b64a299f3ccbb5be73de
Firestar311 <slaterandrew0398@gmail.com>
2024-02-20 12:04:40 -0600

    Added a config utility using BoostedYAML and added saving and loading of colors based on a config option

--- src/main/java/com/stardevllc/starcore/StarCore.java
+++ src/main/java/com/stardevllc/starcore/StarCore.java
@@ -3,17 +3,61 @@ package com.stardevllc.starcore;
 import com.stardevllc.starclock.ClockManager;
 import com.stardevllc.starcore.cmds.StarCoreCmd;
 import com.stardevllc.starlib.task.TaskFactory;
+import com.stardevllc.starmclib.actor.ServerActor;
+import com.stardevllc.starmclib.color.ColorUtils;
+import com.stardevllc.starmclib.color.CustomColor;
 import com.stardevllc.starmclib.task.SpigotTaskFactory;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.UUID;
+
 public class StarCore extends JavaPlugin {
+    
+    private UUID consoleUnqiueId;
+    private Config colorsConfig;
+    
     public void onEnable() {
+        Config config = new Config(new File(getDataFolder(), "config.yml"));
+        config.addDefault("console-uuid", UUID.randomUUID().toString(), "This is the unique id that is assigned to the console.", "Please do not change this manually.");
+        this.consoleUnqiueId = UUID.fromString(config.getString("console-uuid"));
+        config.save();
+        Bukkit.getServer().getServicesManager().register(ServerActor.class, ServerActor.getServerActor(), this, ServicePriority.Highest);
+        
+        config.addDefault("save-colors", false, "This allows the plugin to save colors to colors.yml.", "Colors are defined using the command or by plugins.", "Only colors created by StarCore are saved to the file.");
+        if (config.getBoolean("save-colors")) {
+            this.colorsConfig = new Config(new File(getDataFolder(), "colors.yml"));
+            for (Object key : this.colorsConfig.getSection("colors").getKeys()) {
+                CustomColor customColor = new CustomColor(this);
+                customColor.symbolCode((String) key);
+                customColor.hexValue(config.getString("colors." + key));
+                ColorUtils.addCustomColor(customColor);
+            }
+        }
+        
         getServer().getServicesManager().register(TaskFactory.class, new SpigotTaskFactory(this), this, ServicePriority.Normal);
         ClockManager clockManager = new ClockManager(getLogger(), 50L);
         getServer().getServicesManager().register(ClockManager.class, clockManager, this, ServicePriority.Normal);
         getServer().getScheduler().runTaskTimer(this, clockManager.getRunnable(), 1L, 1L);
 
         getCommand("starcore").setExecutor(new StarCoreCmd(this));
+    }
+
+    @Override
+    public void onDisable() {
+        if (colorsConfig != null) {
+            colorsConfig.set("colors", null);
+            for (CustomColor color : ColorUtils.getCustomColors().values()) {
+                if (color.getOwner().getName().equalsIgnoreCase(getName())) {
+                    colorsConfig.set("colors." + color.getChatCode(), color.getHex());
+                }
+            }
+        }
+    }
+
+    public UUID getConsoleUnqiueId() {
+        return consoleUnqiueId;
     }
 }
