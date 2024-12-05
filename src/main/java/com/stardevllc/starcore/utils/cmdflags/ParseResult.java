42b8ca130dc30ba2b592a7ab567dff9d6ab027c5
Firestar311 <slaterandrew0398@gmail.com>
2024-12-05 12:12:43 -0600

    Added some utility methods for the ParseResult class

--- src/main/java/com/stardevllc/starcore/utils/cmdflags/ParseResult.java
+++ src/main/java/com/stardevllc/starcore/utils/cmdflags/ParseResult.java
@@ -3,4 +3,20 @@ package com.stardevllc.starcore.utils.cmdflags;
 import java.util.Map;
 
 public record ParseResult(String[] args, Map<Flag, Object> flagValues) {
+    public boolean isPresent(Flag flag) {
+        if (flag.type() == FlagType.COMPLEX) {
+            return flagValues.containsKey(flag);
+        } else {
+            Object flagValue = flagValues.get(flag);
+            if (flagValue != null) {
+                return (boolean) flagValue;
+            }
+        }
+        
+        return false;
+    }
+    
+    public Object getValue(Flag flag) {
+        return flagValues.get(flag);
+    }
 }
