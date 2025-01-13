25c4f734cfa530fd44b213404c2d6d9c754d75b3
Firestar311 <slaterandrew0398@gmail.com>
2025-01-13 01:51:41 -0600

    Removed the object tester stuff

--- src/main/java/com/stardevllc/starcore/objecttester/Selector.java
+++
@@ -1,8 +0,0 @@
-package com.stardevllc.starcore.objecttester;
-
-import org.bukkit.command.CommandSender;
-
-@FunctionalInterface
-public interface Selector<T> {
-    T select(CommandSender sender, String[] args);
-}
