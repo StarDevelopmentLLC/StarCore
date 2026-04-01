8e1ea1982b4f6996145ae1f967a39435c5a0b0bd
Firestar311 <slaterandrew0398@gmail.com>
2026-04-01 18:47:29 -0500

    Added a way to parse a plugin key from a String and added a cache to make this faster

--- api/src/main/java/com/stardevllc/registry/PluginKey.java
+++ api/src/main/java/com/stardevllc/registry/PluginKey.java
@@ -1,13 +1,20 @@
 package com.stardevllc.registry;
 
 import com.stardevllc.starlib.objects.key.Key;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This represents a key that is owned by a plugin
  */
 public final class PluginKey implements Key {
+    
+    private static final Map<String, JavaPlugin> CACHE = new HashMap<>();
     
     public static PluginKey of(JavaPlugin plugin, String key) {
         return new PluginKey(plugin, key);
@@ -20,6 +27,39 @@ public final class PluginKey implements Key {
     public PluginKey(JavaPlugin plugin, String key) {
         this.plugin = plugin;
         this.key = key;
+        this.value = plugin.getName().toLowerCase() + ":" + key;
+    }
+    
+    public PluginKey(String str) {
+        String[] split = str.split(":");
+        if (split.length != 2) {
+            throw new IllegalArgumentException("Invalid Format for Plugin Key, must be {pluginName}:{key}");
+        }
+        
+        JavaPlugin javaPlugin;
+        if (!CACHE.containsKey(split[0])) {
+            Plugin plugin = null;
+            for (Plugin p : Bukkit.getPluginManager().getPlugins()) {
+                if (p.getName().equalsIgnoreCase(split[0])) {
+                    plugin = p;
+                    break;
+                }
+            }
+            
+            if (plugin == null) {
+                throw new IllegalArgumentException("Plugin " + split[0] + " does not exist");
+            }
+            
+            if (!(plugin instanceof JavaPlugin jp)) {
+                throw new IllegalArgumentException("Plugin " + plugin.getName() + " is not a JavaPlugin");
+            }
+            this.plugin = jp;
+            CACHE.put(split[0], this.plugin);
+        } else {
+            this.plugin = CACHE.get(split[0]);
+        }
+        
+        this.key = split[1];
         this.value = plugin.getName().toLowerCase() + ":" + key;
     }
     
